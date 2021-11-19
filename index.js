b17f4fd3e51806ce3ff428cba914e991bd406973
Will Brennan <willbrennan314@gmail.com>
2021-11-19 17:18:51 -0500

    commit tree 2d4da39b2d63be5a3ed34ba5413577c7694aae78

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+
+let body = document.querySelector("body")
+
+const userInfo = {
+    name: "Steve",
+    email: "steve@steve.com"
+}
+
+const configurationObject = {
+    method: "POST",
+    headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+    },
+    body: JSON.stringify(userInfo)
+}
+
+function submitData(userName, userMail) {
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(resp => resp.json())
+        .then(data => { 
+            body.append(data.id)
+        })
+        .catch((error) => {
+            body.append(error)
+        })
+}
+
